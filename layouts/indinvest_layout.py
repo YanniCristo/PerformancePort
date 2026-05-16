dd5c6b071c0b41efe4e1284f236db4db600f636e
YanniCristo <fulviopinna1989@tiscali.it>
2026-05-16 21:55:01 +0200

    Update Indinvest

--- layouts/indinvest_layout.py
+++ layouts/indinvest_layout.py
@@ -1,7 +1,6 @@
 import dash_bootstrap_components as dbc
 from components.generic.texts import Nome
 from components.generic.elements import Divisor
-from components.indinvest.paragraph import TitlePar
 from utils.functions import load_content, load_image
 from dash import html, dcc
 
@@ -17,14 +16,22 @@ def indinvest_layout(lang='en'):
         
         html.Div([
             html.H1(txt['parOne']['title']),
-            html.P(''.join(txt['parOne']['description'])),
-            
-            TitlePar(par=txt['parTwo']['description'],
-                     img="assets/contents/indinvest/par1.png", num='01'),
-            TitlePar(par=txt['parTre']['description'],
-                     img="assets/contents/indinvest/par2.png", num='02'),
-            TitlePar(par=txt['parQua']['description'],
-                     img="assets/contents/indinvest/par3.png", num='03'),
+            html.P(txt['parOne']['description'], className="ParOne-Indi"),
+
+            html.Div([
+                html.Div(html.P(txt['parTwo']['description']), className="LeftColumn-Indi"),
+                html.Img(src="assets/contents/indinvest/par1.png", className="Img-Indi"),
+            ], className="Content-Indi"),
+
+            html.Div([
+                html.Div(html.P(txt['parTre']['description']), className="LeftColumn-Indi"),
+                html.Img(src="assets/contents/indinvest/par2.png", className="Img-Indi"),
+            ], className="Content-Indi"),
+
+            html.Div([
+                html.Div(html.P(txt['parQua']['description']), className="LeftColumn-Indi"),
+                html.Img(src="assets/contents/indinvest/par3.png", className="Img-Indi"),
+            ], className="Content-Indi"),
             
         ], className="Cont-indi"),
         
